7ef025e210002a929a0c1b769fea4522cfd5df97
PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-05-07 16:50:52 -0700

    Redid bread.java and added exception for input

---
+++ Bread.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Bread {
+    private String name;
+    private double price;
+
+    // Returns the name of the bread
+    public String getName() {
+        return name;
+    }
+
+    // Returns the price of the bread
+    public double getPrice() {
+        return price;
+    }
+
+    // Validates the user's input and returns the name of the bread
+    public static String select(Scanner scnr) {
+        System.out.println("Please select a bread [1,4].");
+        try {
+            int choice = scnr.nextInt();
+            // Escape code to remove the user's input from terminal (if supported)
+            System.out.print("\033[1A\033[2K");
+            if (choice >= 1 && choice <= 4) {
+                switch (choice) {
+                    case 1:
+                        return "White";
+                    case 2:
+                        return "Wheat";
+                    case 3:
+                        return "French";
+                    case 4:
+                        return "Organic";
+                }
+            }
+            // else
+            System.out.print("Invalid selection. ");
+            return select(scnr);
+        } catch (InputMismatchException e) {
+            System.out.print("Invalid selection. ");
+            scnr.next();
+            return select(scnr);
+        }
+    }
+
+    // Calculates and returns the price of the bread
+    public double getBreadPrice(String choice) {
+        switch (choice) {
+            case "White":
+                return 1.50;
+            case "Wheat":
+                return 1.60;
+            case "French":
+                return 1.80;
+            // case "organic":
+            default:
+                return 2.00;
+        }
+    }
+
+    // Allows me to check if bread name and price matches input
+    public void debugBread() {
+        System.out.printf(
+                "%s bread - $%.2f.\n", getName(), getPrice());
+    }
+}
