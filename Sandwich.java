0f12fc2b271f25c9d1f93ec0761c9781d8b8876a
PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-05-07 01:22:30 -0700

    Created all required classes

--- Sandwich.java
+++ Sandwich.java
@@ -1,8 +1,17 @@
-public class Sandwich extends Bread {
+public class Sandwich {
     private double totalCost;
 
     // Sandwich constructor
     public Sandwich() {
         totalCost = getPrice() + 0.00;
+    }
+
+    // Prints menu of bread options
+    public void showBreadMenu() {
+        System.out.println("=== Select Sandwich Bread: ===\n" +
+                "1. White Bread $1.50\n" +
+                "2. Wheat Bread $1.60\n" +
+                "3. French Bread $1.80\n" +
+                "4. Organic Bread $2.00\n");
     }
 }
---
+++ SandwichApp.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class SandwichApp {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        Sandwich sandwich = new Sandwich();
+    }
+}
