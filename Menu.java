d39b8609f65511ed2102c0699b72dfdd44643e64
PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-05-07 22:06:08 -0700

    minor edits to menu text

--- Menu.java
+++ Menu.java
@@ -1,19 +1,19 @@
 public class Menu {
     public static void showBreadMenu() {
         System.out.println("=== Select Sandwich Bread: ===\n" +
-                "1. White bread $1.50\n" +
-                "2. Wheat bread $1.60\n" +
-                "3. French bread $1.80\n" +
-                "4. Organic bread $2.00\n");
+                "1. White Bread $1.50\n" +
+                "2. Wheat Bread $1.60\n" +
+                "3. French Bread $1.80\n" +
+                "4. Organic Bread $2.00\n");
     }
 
     public static void showVegetablesMenu() {
         System.out.println("=== Select Sandwich Vegetables: ===\n" +
-                "1. Red onions $0.05\n" +
+                "1. Red Onions $0.05\n" +
                 "2. Olives $0.10\n" +
                 "3. Pickles $0.10\n" +
                 "4. Lettuce $0.20\n" +
-                "5. Green peppers $0.25\n" +
+                "5. Green Peppers $0.25\n" +
                 "6. Tomatoes $0.30\n" +
                 "7. Cheese $0.49\n" +
                 "8. Quit vegetable selection\n");
