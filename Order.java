620331874d1d24dc6b7ca5a96ebb60dd3bb756ec
PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-05-07 23:05:55 -0700

    Method to get customer's name

--- Order.java
+++ Order.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Order {
     private Sandwich sandwich;
     private double sandwichCost;
@@ -8,5 +10,13 @@ public class Order {
     public Order(Sandwich sandwich) {
         this.sandwich = sandwich;
         this.sandwichCost = sandwich.getSandwichCost();
+        customerName = enterCustomerName();
+    }
+
+    public String enterCustomerName() {
+        Scanner scnr = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        scnr.close();
+        return scnr.nextLine();
     }
 }
