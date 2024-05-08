b33e7d4684fea965d5e60fdddb6bf0efa81aa9e8
PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-05-07 23:50:06 -0700

    Writes order to file

--- SandwichIO.java
+++ SandwichIO.java
@@ -1,3 +1,16 @@
-public class SandwichIO {
+import java.io.FileWriter;
+import java.io.IOException;
 
+public class SandwichIO {
+    public static void writeOrderToFile(Order order) {
+        try {
+            FileWriter writer = new FileWriter("/Users/btseng/Desktop/CS1400FinalProject/orderline.txt", true);
+            writer.write(order.getContent());
+            writer.write(System.lineSeparator());
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing to the file.");
+            e.printStackTrace();
+        }
+    }
 }
