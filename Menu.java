741d88c1867f1626e5fc6f45a0df53fcd435d522
PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-05-07 17:19:08 -0700

    minor edit to menu text

--- Menu.java
+++ Menu.java
@@ -9,22 +9,22 @@ public class Menu {
 
     public static void showVegetablesMenu() {
         System.out.println("=== Select Sandwich Vegetables: ===\n" +
-                "1 Red onions $0.05\n" +
-                "2 Olives $0.10\n" +
-                "3 Pickles $0.10\n" +
-                "4 Lettuce $0.20\n" +
-                "5 Green peppers $0.25\n" +
-                "6 Tomatoes $0.30\n" +
-                "7 Cheese $0.49\n" +
-                "8 Quit vegetable selection\n");
+                "1. Red onions $0.05\n" +
+                "2. Olives $0.10\n" +
+                "3. Pickles $0.10\n" +
+                "4. Lettuce $0.20\n" +
+                "5. Green peppers $0.25\n" +
+                "6. Tomatoes $0.30\n" +
+                "7. Cheese $0.49\n" +
+                "8. Quit vegetable selection\n");
     }
 
     public static void showMeatMenu() {
         System.out.println("=== Select Sandwich Meats: ===\n" +
-                "1 Ham $1.00\n" +
-                "2 Roasted Chicken Breast $1.10\n" +
-                "3 Turkey Breast $1.20\n" +
-                "4 Roast Beef $1.50\n" +
-                "5 Quit meat selection\n");
+                "1. Ham $1.00\n" +
+                "2. Roasted Chicken Breast $1.10\n" +
+                "3. Turkey Breast $1.20\n" +
+                "4. Roast Beef $1.50\n" +
+                "5. Quit meat selection\n");
     }
 }
