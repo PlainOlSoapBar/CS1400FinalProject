2a96d8f4a8f761f57df6e4e964c73adc541ede5c
PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-05-07 23:50:27 -0700

    Calls the method to write order to file

--- SandwichApp.java
+++ SandwichApp.java
@@ -1,13 +1,25 @@
+import java.util.Scanner;
+
 public class SandwichApp {
     public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+
         boolean continueOrder = true;
         while (continueOrder) {
             // Create a new sandwich
             Sandwich sandwich = new Sandwich();
+
             // Create a new order
             Order order = new Order(sandwich);
 
-            continueOrder = false; // temporary
+            // Print receipt
+            SandwichIO.writeOrderToFile(order);
+
+            // Asks if customer wants to order another sandwich
+            continueOrder = order.orderAgain(scnr);
         }
+
+        System.out.println("\nThank you for ordering!");
+        scnr.close();
     }
 }
