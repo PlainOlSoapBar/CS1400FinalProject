bdf15dbabb583db6f82435afbed367196fbbd473
PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-05-07 22:42:53 -0700

    Meats

---
+++ Meats.java
@@ -0,0 +1,72 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Meats {
+    private String name;
+    private double price;
+
+    // Constructor
+    public Meats(Scanner scnr) {
+        name = select(scnr);
+        price = price(name);
+    }
+
+    // Returns the name of the bread
+    public String getName() {
+        return name;
+    }
+
+    // Returns the price of the bread
+    public double getPrice() {
+        return price;
+    }
+
+    // Validates the user's input and returns the name of the bread
+    public static String select(Scanner scnr) {
+        System.out.println("Please select a meat [1,5].");
+        try {
+            int choice = scnr.nextInt();
+
+            // Escape code to remove the user's input from terminal (if supported)
+            System.out.print("\033[1A\033[2K");
+
+            if (choice >= 1 && choice <= 5) {
+                switch (choice) {
+                    case 1:
+                        return "Ham";
+                    case 2:
+                        return "Roasted Chicken Breast";
+                    case 3:
+                        return "Turkey Breast";
+                    case 4:
+                        return "Roast Beef";
+                    case 5: // Quit
+                        return "";
+                }
+            }
+            // else
+            System.out.print("Invalid selection. ");
+            return select(scnr);
+        } catch (InputMismatchException e) {
+            System.out.print("Invalid selection. ");
+            scnr.next();
+            return select(scnr);
+        }
+    }
+
+    // Calculates and returns the price of the bread
+    public static double price(String choice) {
+        switch (choice) {
+            case "Ham":
+                return 1.00;
+            case "Roasted Chicken Breast":
+                return 1.10;
+            case "Turkey Breast":
+                return 1.20;
+            case "Roast Beef":
+                return 1.50;
+            default: // Quit
+                return 0.00;
+        }
+    }
+}
