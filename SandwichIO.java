6fda72dd6c6375f149398ce53a40480f384f64d0
PlainOlSoapBar <86759315+PlainOlSoapBar@users.noreply.github.com>
2024-05-08 00:09:25 -0700

    replaced path

--- SandwichIO.java
+++ SandwichIO.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 public class SandwichIO {
     public static void writeOrderToFile(Order order) {
         try {
-            FileWriter writer = new FileWriter("/Users/btseng/Desktop/CS1400FinalProject/orderline.txt", true);
+            FileWriter writer = new FileWriter("PATH TO orderline.txt", true);
             writer.write(order.getContent());
             writer.write(System.lineSeparator());
             writer.close();
